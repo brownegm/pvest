bfc980ee111430c4dc041afd16a467c09b231636
brownegm <32085030+brownegm@users.noreply.github.com>
2022-11-21 18:13:32 -0800

    added a return for the function and updated the documentation. added curly brackets to the example for sma_slope

--- R/estParams.R
+++ R/estParams.R
@@ -6,7 +6,13 @@
 #' @param wp.index A numeric. Indicates the index of the data frame including the water potential data
 #' 
 #' @details 
-#'     Data
+#'     Data needs to have species or individual information as well as leaf identifiers. 
+#'     The function takes your "water.potential" column(recommend naming it thus) and a "fresh water content"(again, ideally named fresh.water.content) for multiple 
+#'     hydration states. For information on the production of leaf pressure volume curves see Sack 2011. 
+#'     With the data for each leaf it takes the first for values and estimates a saturated water content, estimates
+#'     relative water content and then with the relationship between relative water deficit and 1/psi, it estimates the 
+#'     osmotic potential and pressure potential at full turgor(max.psip) from which the same parameters are estimated for each
+#'     subsequent hydration state. 
 #' 
 #' @return Returns a data frame with the estimated parameters 
 #' @export
@@ -68,4 +74,5 @@ estParams<-function(data, fw.index, wp.index){
     data_tt[data_tt$unique_id==i, 'cap.tlp.sym']<-rep(leaf_estimate_cap[4], nrow(data_tt[data_tt$unique_id==i,]))
   }
   
+  return(data_tt)
 }
