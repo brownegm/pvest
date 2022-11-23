28edae67e1ba05c49cd90f2c8cb0b44366301b33
brownegm <32085030+brownegm@users.noreply.github.com>
2022-11-23 12:11:47 -0800

    Update documentation

--- R/osmotic_estimates.R
+++ R/osmotic_estimates.R
@@ -5,13 +5,15 @@
 #' @description Estimates the osmotic potential at full turgor from the linear relationship
 #'     between relative water deficit and inverse leaf water potential. 
 #'
-#' @param data a data frame. A data frame containing the data set of the last 4 hydration states for a given leaf 
-#' @param fw.index a double. Data frame index which contains the leaf water potential data
-#' @param wp.index a double. Data frame index which contains the leaf water potential data
+#' @param data A data frame. A data frame containing the data set of the last 4 hydration states for a given leaf 
+#' @param wc.index A double. Data frame index which contains the relative water content/deficit
+#' @param wp.index A double. Data frame index which contains the leaf water potential data
 #'
 #' @return Numeric list containing the slope, intercept and osmotic potential at full turgor, in that order. 
 #'
 #' @export
+#' 
+#' @seealso \code{\link{sma_slope}}, \code{\link{sma_intercept}}
 #'
 
 OsmoticPotFullTurgor <- function(data, fw.index, wp.index) {
@@ -33,38 +35,40 @@ OsmoticPotFullTurgor <- function(data, fw.index, wp.index) {
 NULL
 
 #' Osmotic and pressure parameter estimation
-#' @description Estimate the osmotic potential at full turgor, pressure potential at full turgor for leaves 
-#'     as well as the osmotic potential and pressure potential for each hydration state
-#'     
-#'     This function is written on the values \strong{below} turgor loss point. Here we assume that the data points at
-#'     last 4 hydration states all represent points below turgor loss point. 
-#'     
-#'     It is implemented \strong{after} estimation of leaf saturated water content, relative water content and relative 
-#'     water deficit. @seealso [RelativeWaterCD] for information on the estimation of those parameters. 
-#'     
-#'     Here, we estimate the osmotic potential at full turgor $\pi_{O}$ as the x-intercept of the relationship between
-#'     inverse leaf water potential and relative water deficit below turgor loss point. The slope needs to be negative 
-#'     within the sma_intercept function(@seealso [sma_intercept]) because of the negative inverse of leaf water potential.
-#'     Also, note that unlike in the saturated water content estimation the x values is inverse $\Psi_{leaf}$. 
-#'     
-#'
-#' @param data A data frame. A data frame containing the data set of the last 4 hydration states for a given leaf
-#' @param wp.index A double. A data frame index which contains the leaf water potential data
-#' @param fw.index A double. A data frame index which contains the leaf water potential data
-#' @param n_row A double. Value which indicates the number of rows for estimating parameters. Default to 4 rows. 
-#' @details The data frame must contain four points below turgor loss points. These values of relative water deficit
+#'@description Estimate the osmotic potential at full turgor, pressure potential at full turgor for leaves 
+#'     as well as the osmotic potential and pressure potential for each hydration state.
 #' 
+#' @param data A data frame. A data frame containing the data set of the last 4 hydration states for a given leaf
+#' @param wc.index A double. A data frame index which contains the water content data passed through OsmoticPotFullTurgor function
+#' @param wp.index A double. A data frame index which contains the leaf water potential data
+#' @param n_row A double. Value which indicates the number of rows for estimating parameters. Default to 4 rows. 
+#'     
+#'@details
+#'This function estimates osmotic variables from the values \strong{below} turgor loss point. Here we assume that the data points at
+#'last 4 hydration states all represent points below turgor loss point. 
+#'     
+#'It is implemented \strong{after} estimation of leaf saturated water content, relative water content and relative 
+#'water deficit.See \code{\link{RelativeWaterCD}} `RelativeWaterCD` for information on the estimation of those parameters. 
+#'     
+#'Here, we estimate the osmotic potential at full turgor (\code{\pi_{O}}) `pi_{O}`as the x-intercept of the relationship between
+#'inverse leaf water potential and relative water deficit below turgor loss point. The slope needs to be negative 
+#'within the sma_intercept function(\code{\link{sma_intercept}}) because of the negative inverse of leaf water potential.
+#'Also, note that unlike in the saturated water content estimation the x values is inverse \code{\Psi_{leaf}}. 
+#'
+#'      
 #' @return Returns data frame with new columns containing the osmotic and pressure potential variables namely: 
-#'     * osmotic potential at full turgor
-#'     * maximum pressure potential 
-#'     * osmotic potential (for each hydration state)
-#'     * pressure potential (for each hydration state)
-#'     * apoplastic fraction
-#'     * symplastic relative water content(sym.rwc; for each hydration state)
+#'    \itemize{
+#'     \item osmotic potential at full turgor
+#'     \item maximum pressure potential 
+#'     \item osmotic potential (for each hydration state)
+#'     \item pressure potential (for each hydration state)
+#'     \item apoplastic fraction
+#'     \item symplastic relative water content(sym.rwc; for each hydration state)
+#'     }
 #'
 #' @import dplyr 
 #' @export OsmoticEstimates
-#'
+#' @seealso [RelativeWaterCD()], [sma_intercept()]
 
 OsmoticEstimates<- function(data, fw.index, wp.index, n_row=4) {
    
