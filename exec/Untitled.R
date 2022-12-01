13fa90601fbcabc8f620e5e8e4d5d704985ccd68
brownegm <32085030+brownegm@users.noreply.github.com>
2022-11-30 21:50:46 -0800

    functions run without error technically but some have NAs or weird estimates, need to check

--- exec/Untitled.R
+++ exec/Untitled.R
@@ -6,10 +6,11 @@
 
 library(tidyverse)
 library(here)
+library(readxl)
 
 #load_data
 load("~/Documents/Research_UCLA/ITV-PV/data/itvdat_2022-07-16_.RData")
-pv_dat<-read_excel(here("inst","pvldcurvedata_10042022.xlsx"))%>%
+pv_dat<-read_excel(here("inst","extdata", "pvldcurvedata_10042022.xlsx"))%>%
   select(!c(dry.weight, fresh.weight.saturated, water.potential.bar, bag.weight,sp.leaf))%>%as.data.frame()
 
 #compute parameter estimates
@@ -18,4 +19,5 @@ pv_params<-estParams(pv_dat, fw.index = 4, wp.index = 3)
 #summarize by species
 param_sum<-sumParams(pv_params, species)
 
+write.csv(param_sum, "sum_params.csv")
 
