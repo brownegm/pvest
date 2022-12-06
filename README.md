9de869b621bc4ce4c2cb6daa260fe7afa0f2741b
brownegm <32085030+brownegm@users.noreply.github.com>
2022-12-06 13:55:54 -0800

    combine estimates with the hand measured values
    don't match tremendously well but tbd

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project aims to automate the determination of pressure volume curve parameters using intrinsic relationships among parameters to draw lines and predict saturated water content, osmotic potential at full turgor and leaf water potential at turgor loss. The automated prediction of these parameters specifically reduce the inter-investigator disparity that may occur and allows for the rapid determination of all pressure volume curve parameters for large data sets.
 
 Pressure volume curves are created by repeated measurements of leaf fresh mass and leaf water 
-potential( $\Psi_L$;i.e., sum of leaf solute($\Psi_s$ ) and pressure( $\Psi_p$) potentials) 
+potential( $\Psi_L$;i.e., sum of leaf solute($\Psi_s$) and pressure( $\Psi_p$) potentials) 
 across dehydration states (ideally, at 0.2-0.3 MPa intervals).
 
 ## Usage
--- exec/Untitled.R
+++ exec/Untitled.R
@@ -9,14 +9,28 @@ library(here)
 library(readxl)
 
 #load_data
-load("~/Documents/Research_UCLA/ITV-PV/data/itvdat_2022-07-16_.RData")
+#load("~/Documents/Research_UCLA/ITV-PV/data/itvdat_2022-07-16_.RData")
 pv_dat<-read_excel(here("inst","extdata", "pvldcurvedata_10042022.xlsx"))%>%
   select(!c(dry.weight, fresh.weight.saturated, water.potential.bar, bag.weight,sp.leaf))%>%as.data.frame()
 
 #compute parameter estimates
 pv_params<-estParams(pv_dat, fw.index = 4, wp.index = 3)
 
+pv_params_byleaf<-pv_params%>%  
+  select(species, leaf, unique_id, saturated.water.content, osm.pot.fullturgor,apoplastic.fraction, 
+                                       relative.water.deficit.attlp:cap.tlp.sym)%>%
+  group_by(unique_id)%>%summarize_if(is.numeric, unique)
+
+write.csv(pv_params_byleaf,here('inst/extdata', 'leafparams_sum.csv'), row.names = F)
+
+uniques<-read_excel(here("inst","extdata","summary_main.xlsx"), sheet = "uniques")%>%pull(unique_id)
+
+pv_leaf_uniques<-pv_params_byleaf%>%
+  filter(unique_id%in%uniques)
+
+write.csv(pv_leaf_uniques, here("inst/extdata/pv_uniques.csv"), row.names=F)
 #summarize by species
+
 param_sum<-sumParams(pv_params, species)
 
 write.csv(param_sum, "sum_params.csv")
