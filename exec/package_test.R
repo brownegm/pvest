aabbc8e1386dfeb9727bcfb5ea76f9f0cbf7a28c
brownegm <32085030+brownegm@users.noreply.github.com>
2023-05-18 09:31:25 -0700

    Update package_test.R
    working on estimating line fits

--- exec/package_test.R
+++ exec/package_test.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(here)
 library(readxl)
 library(readr)
+library(pvest)
 
 # load data ---------------------------------------------------------------
 
@@ -62,28 +63,105 @@ com<-left_join(itvpv, pv_params_byleaf, by="unique_id", suffix = c("", "_est"))
 #figure this out!!!
 #com_sp<-left_join(itvpv_sum, param_sum, by=species==spcode, suffix = c("", "_est"))
 
+
+# Estimate prediction fits and intervals ----------------------------------
+
+og_vars<-names(com)[9:20]
+pred_vars<-names(com)[c(22:24, 26, 28:35)]
+#reorder pred_vars to match og_vars
+pred_vars<-pred_vars[c(1,2,6,3:5,7:8,9,11,10,12)]
+
+
+pv_lms<-purrr::map2(og_vars, pred_vars, \(og,pred) lm(as.formula(og~pred), data = com))
+
+pv_lms<-mapply(og_vars, pred_vars, \(og,pred) lm(as.formula(og~pred), data = com))
+
+model.ols.Dt <- lm(Dtobserved~Dtestimated-1, data=data.trichome.surface.factor)
+summary(model.ols.Dt)
+
+model.ols.Ds <- lm(Dsoberved~Dsestimated-1, data=data.trichome.surface.factor)
+summary(model.ols.Ds)
+
+newdata <- data.frame(data.trichome.surface.factor$Dtestimated)
+newdataDs <- data.frame(data.trichome.surface.factor$Dsestimated)
+
+newx <- seq(0, 450, by=1)
+newy <- seq(0, 900, by=1)
+
+Dtestimated <- data.trichome.surface.factor$Dtestimated
+Dsestimated <- data.trichome.surface.factor$Dsestimated
+
+pred_interval.Dt <- predict(model.ols.Dt, newdata=data.frame(Dtestimated=newx), interval="prediction", level=0.95)
+pred_interval.Ds <- predict(model.ols.Ds, newdata=data.frame(Dsestimated=newy), interval="prediction", level=0.95)
+model.ols.Dt <- lm(Dtobserved~Dtestimated-1, data=data.trichome.surface.factor)
+summary(model.ols.Dt)
+
+
+model.ols.Ds <- lm(Dsoberved~Dsestimated-1, data=data.trichome.surface.factor)
+summary(model.ols.Ds)
+
+newdata <- data.frame(data.trichome.surface.factor$Dtestimated)
+newdataDs <- data.frame(data.trichome.surface.factor$Dsestimated)
+
+newx <- seq(0, 450, by=1)
+newy <- seq(0, 900, by=1)
+
+Dtestimated <- data.trichome.surface.factor$Dtestimated
+Dsestimated <- data.trichome.surface.factor$Dsestimated
+
+pred_interval.Dt <- predict(model.ols.Dt, newdata=data.frame(Dtestimated=newx), interval="prediction", level=0.95)
+pred_interval.Ds <- predict(model.ols.Ds, newdata=data.frame(Dsestimated=newy), interval="prediction", level=0.95)
+
 # plot things -------------------------------------------------------------
 pdf(file=here::here("inst/extdata", "pv_params_leaf.pdf"))
 
-plot(com$swc,com$saturated.water.content)
+plot(com$swc,com$saturated.water.content, 
+     pch=21, col="black",bg= "green")
 
-plot(com$pi_o,com$osm.pot.fullturgor)
+abline(a=0, b=1, lwd=2)
 
-plot(com$psi_tlp,com$leaf.waterpotential.attlp)
+plot(com$pi_o,com$osm.pot.fullturgor, 
+     pch=21, col="black",bg= "green")
 
-plot(com$af,com$apoplastic.fraction)
+abline(a=0, b=1, lwd=2)
 
-plot(com$rwc_tlp,com$relative.water.content.attlp)
+plot(com$psi_tlp,com$leaf.waterpotential.attlp, 
+     pch=21, col="black",bg= "green")
 
-plot(com$modulus,com$modulus_est)
+abline(a=0, b=1, lwd=2)
 
-plot(com$mod_sym,com$modulus_sym)
+plot(com$af,com$apoplastic.fraction, 
+     pch=21, col="black",bg= "green")
 
-plot(com$cap_ft,com$cap.ft.bulk)
+abline(a=0, b=1, lwd=2)
 
-plot(com$cap_tlp,com$cap.tlp.bulk)
+plot(com$rwc_tlp,com$relative.water.content.attlp, 
+     pch=21, col="black",bg= "green")
+
+abline(a=0, b=1, lwd=2)
+
+plot(com$modulus,com$modulus_est, 
+     pch=21, col="black",bg= "green")
+
+abline(a=0, b=1, lwd=2)
+
+plot(com$mod_sym,com$modulus_sym, 
+     pch=21, col="black",bg= "green")
+
+abline(a=0, b=1, lwd=2)
+
+plot(com$cap_ft,com$cap.ft.bulk, 
+     pch=21, col="black",bg= "green")
+
+abline(a=0, b=1, lwd=2)
+
+plot(com$cap_tlp,com$cap.tlp.bulk, 
+     pch=21, col="black",bg= "green")
+
+abline(a=0, b=1, lwd=2)
 
 dev.off()
+
 
 # output results ----------------------------------------------------------
 
