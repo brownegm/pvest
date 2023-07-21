0764f0599f06e79b87f5d3561a6acb5081f647fa
brownegm <32085030+brownegm@users.noreply.github.com>
2023-07-21 14:14:16 -0400

    Update misc_parameters_fortesting.R

--- exec/misc_parameters_fortesting.R
+++ exec/misc_parameters_fortesting.R
@@ -1,16 +1,25 @@
 
 
-data<-pv_dat%>%filter(species=="acne"& leaf=="1")
+roco1<-pv_dat%>%filter(species=="roco" & leaf=="1")
+
+data<-pv_dat%>%filter(species=="roco"& leaf=="4")
+
+data<-pv_dat%>%filter(species=="alma"& leaf=="2")
 
 fw.index = 5; wp.index = 4; dm.index = 3; n_pts = T
 
+# ROCO 1 & 4 , ALMA 2 problems as of 20230719
 
 i<-unique_ids[1]
 
-
+plot(leaf_estimate$relative.water.deficit, leaf_estimate$inv.water.potential)
 leaf_estimate
 
 t<-check_n_pts(leaf_estimate, wp.index="inv.water.potential", wm.index="relative.water.deficit")#$cv10
 
 
 pv_params_fil<-pv_params%>%filter(is.na(modulus))
+
+plot
+
+kl
