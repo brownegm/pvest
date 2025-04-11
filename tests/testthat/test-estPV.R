bd98bb4f46b7dea0874016b9844e714fc9964015
brownegm <32085030+brownegm@users.noreply.github.com>
2025-04-11 12:56:38 -0700

    checks work; new vignette

--- tests/testthat/test-estPV.R
+++ tests/testthat/test-estPV.R
@@ -1,17 +1,20 @@
 test_that("validate inputs", {
 
-  data <- pvest::quag 
-
   #wrong column name in the input
-  expect_error(estPV(data, species, leaf, fresh.rain, water.potential, hot.weight))
+  expect_error(estPV(data = pvest::quag, species, leaf, fresh.rain, water.potential, hot.weight))
   
   #inspect output
-  ah <- estPV(pvest::quag, species, subgrp = leaf, fw = fresh.weight, water.potential, dry.weight)
+  default <- estPV(pvest::quag, species, subgrp = leaf, fw = fresh.weight, water.potential, dry.weight)
   
+  expect_equal(length(default), 5) # all things are outputted
+
   expect_error(estPV(pvest::quag, species, subgrp = leaf, fw = fresh.weight, water.potential, dry.weight, method = "justguess"))
-  expect_equal(unique(ah[[1]]$species), "quag_gj")
-  expect_equal(names(ah[[1]])[3], "leaf")
-  expect_equal(length(ah), length(unique(paste0(pvest::quag$species, pvest::quag$leaf))))
+  expect_equal(unique(default[[1]]$species), "quag_gj")
+  expect_equal(names(default[[1]])[3], "leaf")
+  expect_equal(length(default), length(unique(paste0(pvest::quag$species, pvest::quag$leaf))))
   
   
+  #check method use
+  rmse <- estPV(pvest::quag, species, subgrp = leaf, fw = fresh.weight, water.potential, dry.weight, method = "rmse")
+  r2 <- estPV(pvest::quag, species, subgrp = leaf, fw = fresh.weight, water.potential, dry.weight, method = "r2")
 })
