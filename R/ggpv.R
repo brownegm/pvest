fedafacc6bee5c4895da37f39995f13b6df45c59
brownegm <32085030+brownegm@users.noreply.github.com>
2025-04-27 17:06:06 -0700

    new stat fx name

--- R/ggpv.R
+++ R/ggpv.R
@@ -1,13 +1,12 @@
 #' Stat function for plotting pv curve on data
 
-#'@importFrom ggplot2 ggplot ggproto
+#'@importFrom ggplot2 ggplot ggproto Stat
 #@inheritParams ggplot2::stat_smooth
 #'@export
 
 ## Create ggproto object
 
-statPV <- ggproto("statPV", Stat, 
+StatPV <- ggplot2::ggproto("StatPV", ggplot2::Stat, 
             compute_group = function(pvest){
-              
               pvStats <- pvest$stat
             } )
