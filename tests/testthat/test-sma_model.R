e6be7b268c4fa48fe3f2ddc92c1e28fc59bb789e
brownegm <32085030+brownegm@users.noreply.github.com>
2025-11-30 12:08:51 -0800

    fixed function name

--- tests/testthat/test-sma_model.R
+++ tests/testthat/test-sma_model.R
@@ -8,15 +8,24 @@ test_that("Check input values for NAs", {
   a1 <- c(1, 2, 3, 4, 5)
   b1 <- c(1, 2, 3, 4, 5)
   c1 <- c(1, 2, 3, 4, 5)
-  oinput <- pvest:::osminput(x, y)
+  oinput <- pvest:::new_osminput(x, y)
   tinput <- pvest:::tlpinput(x, y, z, a, b, c, a1, b1, c1)
 
-  expect_error(sma_model(x, y), "sma_model:Missing values found in input data. Ensure that there are no missing values.")
+  expect_error(
+    sma_model(x, y),
+    "sma_model:Missing values found in input data. Ensure that there are no missing values."
+  )
 
-  expect_error(sma_model.osm_input(x = oinput), "sma_model:Missing values found in input data. Ensure that there are no missing values.")
+  expect_error(
+    sma_model.osm_input(x = oinput),
+    "sma_model:Missing values found in input data. Ensure that there are no missing values."
+  )
 
-  expect_error(calc_param_tlp(x = tinput), "calc_param_tlp:Missing values found in input data. Ensure that there are no missing values.")
-  
+  expect_error(
+    calc_param_tlp(x = tinput),
+    "calc_param_tlp:Missing values found in input data. Ensure that there are no missing values."
+  )
+
   #expect_snapshot(print(sma_model(x,z)))
 })
 
