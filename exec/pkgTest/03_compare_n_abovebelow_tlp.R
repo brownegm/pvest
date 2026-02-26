ba31c16f465e82f3ae74164c3c7404488b567612
brownegm <32085030+brownegm@users.noreply.github.com>
2026-02-26 10:57:40 -0800

    turned 04_diagnose* into a rmd.. found a source of error

--- exec/pkgTest/03_compare_n_abovebelow_tlp.R
+++ exec/pkgTest/03_compare_n_abovebelow_tlp.R
@@ -1,4 +1,4 @@
-# Determine number of points for estimates that are not one to one. 
+# Determine number of points for estimates that are not one to one.
 
 library(ggplot2)
 library(pvest)
@@ -32,20 +32,21 @@ plot_comp_cols <- function(data, x, y) {
       aes(label = paste(..eq.label.., ..rr.label.., sep = "~~")),
       formula = y ~ x,
       parse = TRUE,
-      size = 5)
-    # )+
-    # scale_fill_woodcut(palette = "earth_cb", discrete = T)
-  
+      size = 5
+    )
+  # )+
+  # scale_fill_woodcut(palette = "earth_cb", discrete = T)
+
   return(p)
 }
 
 # Plot comparison color by n points ---------------------------------------
 
 com <- combined_manual_auto_estimates # for simplicity in naming
-com_n_zeros <- com |> 
-  group_by(ids) |> 
-  mutate(n_zero = sum(prespot == 0, na.rm = TRUE)) |> 
-  filter(!n_zero==0)
+com_n_zeros <- com |>
+  group_by(ids) |>
+  mutate(n_zero = sum(prespot == 0, na.rm = TRUE)) |>
+  filter(!n_zero == 0)
 
 
 pcpv2 <- plot_comp_cols(com_n_zeros, "pio", "pi_o") +
@@ -67,20 +68,49 @@ pcpv5 <- plot_comp_cols(com_n_zeros, "rwc_tlp_est", "rwc_tlp") +
   pcpv4,
   pcpv5,
   ncol = 4,
-  axis_titles = "collect", 
+  axis_titles = "collect",
   guides = "collect"
 ))
 
-ggsave(filename = here("inst/extdata/pv_color_n_zeros.pdf"), pcpvall, dpi = 500, width = 10, height = 5, units = "in")
-
+ggsave(
+  filename = here("inst/extdata/pv_color_n_zeros.pdf"),
+  pcpvall,
+  dpi = 500,
+  width = 10,
+  height = 5,
+  units = "in"
+)
 
 
 # look at codi2
 
-codi2 <- com |> 
-  filter(ids =="codi_6")
+codi2 <- com |>
+  filter(ids == "codi_6")
 
 
-(codi2_hof <- codi2 |> 
-  ggplot2::ggplot(aes(x = rwd, y = water.potential))+
+(codi2_hof <- codi2 |>
+  ggplot2::ggplot(aes(x = rwd, y = water.potential)) +
   geom_point())
+
+
+com <- combined_manual_auto_estimates
+
+com_err <- com |>
+  mutate(
+    pio_err = pio - pi_o,
+    pitlp_err = pi_tlp - psi_tlp,
+    pio_rmse = sqrt(pio_err^2),
+    pitlp_rmse = sqrt(pitlp_err^2),
+  ) |>
+  arrange(desc(abs(pio_err))) |>
+  select(
+    spcind,
+    pio,
+    pi_o,
+    pio_err,
+    pio_rmse,
+    pi_tlp,
+    psi_tlp,
+    pitlp_err,
+    pitlp_rmse
+  )
