a9afb5c558b8c8b0e578b797d5de8a25fb6c4470
brownegm <32085030+brownegm@users.noreply.github.com>
2026-03-20 12:36:32 -0700

    typo check

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' \item{species}{Species code: first two letters of Genus and Species}
 #' \item{leaf}{Leaf number}
 #' \item{dry.weight}{Dry mass of leaf in grams}
-#' \item{water.potential}{:eaf water potential in MPa}
+#' \item{water.potential}{Leaf water potential in MPa}
 #' \item{fresh.weight}{Leaf water weight in grams}
 #' }
 
